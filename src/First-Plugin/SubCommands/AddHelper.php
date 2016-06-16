8a7d09665c209892e4a84cb39b1b9939c015b6ce
AllMCPE <dominickthomassmith@gmail.com>
2016-06-16 14:39:04 -0500

    First Commit
    Not finished yet!

--- src/First-Plugin/SubCommands/AddHelper.php
+++ src/First-Plugin/SubCommands/AddHelper.php
@@ -1,1 +1,12 @@
+<?php
+namespace First-Plugin\SubCommands;
 
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
+
+class AddHelper extends SubCommands
+{
+    public function CanUse(CommandSender $sender) {
+        return ($sender instanceof Player) and $sender->HasPermission("firstplugin.addhelper")
+    }
