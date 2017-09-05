9f3c04d69c2e25c60cace5fa99da18678940530d
AllMCPE <dominickthomassmith@gmail.com>
2017-09-04 19:50:32 -0500

    Update and rename announce.php to Announce.php

---
+++ src/First-Plugin/commands/Announce.php
@@ -0,0 +1,8 @@
+namespace First-Plugin\commands;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\command\utils\InvalidCommandSyntaxException;
+use pocketmine\event\TranslationContainer;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
